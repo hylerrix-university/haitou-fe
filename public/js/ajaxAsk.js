af5ab37cece539c8875394774937d096f078f7ff
hylerrix <hylerrix@foxmail.com>
2017-05-24 15:15:47 +0800

    完成度90%只剩详情页

--- public/js/ajaxAsk.js
+++ public/js/ajaxAsk.js
@@ -40,7 +40,7 @@ function setTalkInfor () {
 				xmlhttp.open("GET", url, true);
 			    xmlhttp.send();
 		    } else {
-		    	rmLoadingInfor();
+		    	removeChildByClassName("xjhItemTeamWrap", 0, "xmlLoading", 0);
 		    	console.log(currentPage + " 个页面资源获取成功");
 		    	renderTalkInfor(totalTalkInfor);
 		    }
@@ -54,19 +54,24 @@ function setTalkInfor () {
 
 /**
  * 渲染宣讲会信息
- * @param {Array} totalTalkInfor
+ * @param {Object} totalTalkInfor
  */
 function renderTalkInfor (totalTalkInfor) {
 	var totalPageNum = totalTalkInfor.length;
 	var totalTalkNum = 0;
-    var pn = document.getElementsByClassName("xjhItemTeamWrap")[0];
-    var op;
+    var parentNode = document.getElementsByClassName("xjhItemTeamWrap")[0];
+    var dateWrapNum = 0;
 
  	for (var i = 0; i < totalPageNum; i++) {
 		for (var j = 0; j < totalTalkInfor[i].rows.length; j++) {
 			var currentTalk = totalTalkInfor[i].rows[j];
-			op = document.createElement("div");
-			op.innerHTML = "\
+			var perTalk = document.createElement("div");
+			var dateWrap = document.createElement("div");
+
+			dateWrap.setAttribute("class", "xjhItemTeamDateWrap");
+			dateWrap.innerHTML = "2017-05-24 周三";
+
+			perTalk.innerHTML = "\
 				<a>\
 				    <!-- 某一场宣讲会 -->\
 					<div class=\"xjhItemWrap\">\
@@ -93,7 +98,9 @@ function renderTalkInfor (totalTalkInfor) {
 				    </div>\
 				</a>\
 			";
-		    pn.appendChild(op);
+			parentNode.appendChild(dateWrap);
+			parentNode.appendChild(perTalk);
+
 			var xjhItemLogoWrap = document.getElementsByClassName("xjhItemLogoWrap")[totalTalkNum];
 	    	var xjhItemCompanyName = document.getElementsByClassName("xjhItemCompanyName")[totalTalkNum];
 	    	var xjhItemSchoolName = document.getElementsByClassName("xjhItemSchoolName")[totalTalkNum];
@@ -110,24 +117,54 @@ function renderTalkInfor (totalTalkInfor) {
 	    	xjhItemWeek.innerHTML = currentTalk.holdTime;
 	    	xjhItemTimePoint.innerHTML = "(近期)";
 
+	    	var childIndex = document.getElementsByClassName("xjhItemTeamDateWrap").length - 1;
+	    	var xjhItemTeamDateWrap = document.getElementsByClassName("xjhItemTeamDateWrap")[childIndex];
+		    xjhItemTeamDateWrap.innerHTML = currentTalk.holdTime.substring(0, 10);
+
+	    	if (totalTalkNum >= 1) {
+	    		var xjhItemTeamDateWrapBefore = document.getElementsByClassName("xjhItemTeamDateWrap")[dateWrapNum];
+		    	if (xjhItemTeamDateWrap.innerHTML === xjhItemTeamDateWrapBefore.innerHTML) {
+		    		removeChildByClassName("xjhItemTeamWrap", 0, "xjhItemTeamDateWrap", childIndex);
+		    	} else {
+		    		dateWrapNum++;
+		    	}
+	    	}
+
 	    	totalTalkNum++;
 	    }
 	}
 
 	console.log(totalTalkNum + " 条宣讲会信息填充成功");
+	console.log(dateWrapNum + " 天的信息加载成功");
 
-	op = document.createElement("div");
-	op.setAttribute('class', 'loadMore');
-	op.innerHTML = "加载完成，共 " + totalTalkNum + " 条宣讲会信息";
-	pn.appendChild(op);
+	var doneWrap = document.createElement("div");
+	doneWrap.setAttribute('class', 'loadMore');
+	doneWrap.innerHTML = "加载完成，共 " + totalTalkNum + " 条宣讲会信息";
+	parentNode.appendChild(doneWrap);
 }
 
-function rmLoadingInfor () {
-    var parent = document.getElementsByClassName("xjhItemTeamWrap")[0];
-	var xmlLoading = document.getElementsByClassName("xmlLoading")[0];
-	parent.removeChild(xmlLoading);
+/**
+ * 渲染详情页
+ * @param {Array} detailInfor
+ */
+function renderDetailInfor (detailInfor) {
+	;
+}
+
+/**
+ * 删除结点
+ * @param {string} parent
+ * @param {number} parentIndex
+ * @param {string} child
+ * @param {number} childIndex
+ */
+function removeChildByClassName (parent, parentIndex, child, childIndex) {
+    var parent = document.getElementsByClassName(parent)[parentIndex];
+	var child = document.getElementsByClassName(child)[childIndex];
+	parent.removeChild(child);
 }
 
 window.onload = function () {
 	setTalkInfor();
 }
+
