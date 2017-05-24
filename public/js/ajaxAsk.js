1b4c237a4351eee2f02c4472bd2c445b71a51bd1
hylerrix <hylerrix@foxmail.com>
2017-05-24 12:28:14 +0800

    完成首页所有信息自动载入模块

--- public/js/ajaxAsk.js
+++ public/js/ajaxAsk.js
@@ -1,15 +1,15 @@
+
+
 /**
- * Ajax 跨域核心函数
- * @param {string} url - 跨域路由
- * @param {string} cfunc - 具体需要执行的函数
+ * 获取宣讲会信息
+ * setTalkInfor 内部分为 get 和 render 方法
+ * @return {string} talkInfo
  */
+function setTalkInfor () {
+	// talkInfor 保存当前省份的所有宣讲会信息
+	var totalTalkInfor = [];
 
-var provinceInfor = "";
-var talkInfor = "";
-var article = "";
-
-function loadXMLDoc (url, cfunc) {
-    if (window.XMLHttpRequest) {
+	if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
 	    xmlhttp = new XMLHttpRequest();
 	} else {
@@ -17,9 +17,110 @@ function loadXMLDoc (url, cfunc) {
 	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
 
-    xmlhttp.onreadystatechange = cfunc;
+	// 默认必须先请求第一页
+	var currentPage = 1;
+	var url = "https://javaapi.changxiaoyuan.com/ht/infor/getTalkInfor.action?province=xa&pageNum=" + currentPage;
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
+
+    xmlhttp.onreadystatechange = function () {
+    	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+    		// 获取到所请求的页面宣讲会信息并 JSON 化
+	    	var currentTalkInfor = xmlhttp.responseText;
+	    	var currentTalk = JSON.parse(currentTalkInfor);
+		    // 获取到总页面数 totalPageNum
+		    var totalPageNum = currentTalk.totalPageNum;
+		    // 将当前页面的宣讲会列表和个数传递到 addTalkInfor
+		    // console.log(currentTalk.rows);
+		    addTalkInfor(currentTalk);
+
+		    // 判断是否进行下一个页面的请求
+		    if (currentPage < totalPageNum) {
+		    	currentPage++;
+		    	url = "https://javaapi.changxiaoyuan.com/ht/infor/getTalkInfor.action?province=xa&pageNum=" + currentPage;
+				xmlhttp.open("GET", url, true);
+			    xmlhttp.send();
+		    } else {
+		    	console.log(currentPage + " 个页面资源获取成功");
+		    	renderTalkInfor(totalTalkInfor);
+		    }
+  		}
+  	}
+
+  	function addTalkInfor (currentInforJSON) {
+  		totalTalkInfor.push(currentInforJSON);
+  	}
+}
+
+/**
+ * 渲染宣讲会信息
+ * @param {Array} totalTalkInfor
+ */
+function renderTalkInfor (totalTalkInfor) {
+	var totalPageNum = totalTalkInfor.length;
+	var totalTalkNum = 0;
+    var pn = document.getElementsByClassName("xjhItemTeamWrap")[0];
+    var op;
+
+ 	for (var i = 0; i < totalPageNum; i++) {
+		for (var j = 0; j < totalTalkInfor[i].rows.length; j++) {
+			var currentTalk = totalTalkInfor[i].rows[j];
+			op = document.createElement("div");
+			op.innerHTML = "\
+				<a>\
+				    <!-- 某一场宣讲会 -->\
+					<div class=\"xjhItemWrap\">\
+				    	<div class=\"xjhItemLogoWrap\">\
+				    		<img src=\"public/chang-logo.jpeg\">\
+				    	</div>\
+				    	<div class=\"xjhItemContentWrap\">\
+				    		<div class=\"xjhItemCompanyName\">公司名</div>\
+				    		<div class=\"xjhItemSchoolInfoWrap\">\
+				    			<div class=\"xjhItemSchoolName\">大学名</div>\
+				    			<div class=\"xjhItemSchoolAddress\">宣讲地点</div>\
+				    		</div>\
+				    		<div class=\"xjhItemTimeWrap\">\
+				    		    <div class=\"xjhItemWeek\">宣讲时间</div>\
+				    			<div class=\"xjhItemTimePoint\">时间状况</div>\
+				    		</div>\
+				    	</div>\
+				    	<div class=\"clear\"></div>\
+				    	<div class=\"xjhItemPointImgWrap\">\
+				    	</div>\
+				    	<div class=\"xjhSaveBtn\">\
+				    		<img src=\"\">\
+				    	</div>\
+				    </div>\
+				</a>\
+			";
+		    pn.appendChild(op);
+			var xjhItemLogoWrap = document.getElementsByClassName("xjhItemLogoWrap")[totalTalkNum];
+	    	var xjhItemCompanyName = document.getElementsByClassName("xjhItemCompanyName")[totalTalkNum];
+	    	var xjhItemSchoolName = document.getElementsByClassName("xjhItemSchoolName")[totalTalkNum];
+	    	var xjhItemSchoolAddress = document.getElementsByClassName("xjhItemSchoolAddress")[totalTalkNum];
+	    	var xjhItemWeek = document.getElementsByClassName("xjhItemWeek")[totalTalkNum];
+	    	var xjhItemTimePoint = document.getElementsByClassName("xjhItemTimePoint")[totalTalkNum];
+
+	    	xjhItemLogoWrap.children[0].src = "public/chang-logo.jpeg";
+	    	xjhItemCompanyName.innerHTML = currentTalk.companyName;
+	    	xjhItemSchoolName.innerHTML = "西安";
+	    	// currentTalk.xjhItemSchoolName
+	    	xjhItemSchoolAddress.innerHTML = currentTalk.place;
+			// currentTalk.xjhItemSchoolAddress
+	    	xjhItemWeek.innerHTML = currentTalk.holdTime;
+	    	xjhItemTimePoint.innerHTML = "(近期)";
+
+	    	totalTalkNum++;
+	    }
+	}
+
+	console.log(totalTalkNum + " 条宣讲会信息填充成功");
+
+	op = document.createElement("div");
+	op.setAttribute('class', 'loadMore');
+	op.innerHTML = "加载完成，共 " + totalTalkNum + " 条宣讲会信息";
+	pn.appendChild(op);
 }
 
 /**
@@ -29,81 +130,6 @@ function getProvince () {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
     	provinceInfor = xmlhttp.responseText;
     }
-}
-
-/**
- * 获取宣讲会信息
- */
-function getTalkInfor () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    	var talkInfor = xmlhttp.responseText;
-    	renderTalkInfor(talkInfor);
-    }
-}
-
-/**
- * 渲染宣讲会信息
- * @param {string} talkInfor
- */
-function renderTalkInfor (talkInfor) {
-    var talkJSON = JSON.parse(talkInfor);
-    var talkNum = talkJSON.rows.length;
-
-    var pn = document.getElementsByClassName("xjhItemTeamWrap")[0];
-    var op;
-
- 	for (var i = 0; i < talkNum; i++) {
-		op = document.createElement("div");
-		op.innerHTML = "\
-			<a>\
-			    <!-- 某一场宣讲会 -->\
-				<div class=\"xjhItemWrap\">\
-			    	<div class=\"xjhItemLogoWrap\">\
-			    		<img src=\"public/chang-logo.jpeg\">\
-			    	</div>\
-			    	<div class=\"xjhItemContentWrap\">\
-			    		<div class=\"xjhItemCompanyName\">全国中小企业股份转让系统公司</div>\
-			    		<div class=\"xjhItemSchoolInfoWrap\">\
-			    			<div class=\"xjhItemSchoolName\">西工大</div>\
-			    			<div class=\"xjhItemSchoolAddress\">长安校区就业信息发布厅</div>\
-			    		</div>\
-			    		<div class=\"xjhItemTimeWrap\">\
-			    		    <div class=\"xjhItemWeek\">2017-05-17 13:30</div>\
-			    			<div class=\"xjhItemTimePoint\">(今天)</div>\
-			    		</div>\
-			    	</div>\
-			    	<div class=\"clear\"></div>\
-			    	<div class=\"xjhItemPointImgWrap\">\
-			    	</div>\
-			    	<div class=\"xjhSaveBtn\">\
-			    		<img src=\"\">\
-			    	</div>\
-			    </div>\
-			</a>\
-		";
-	    pn.appendChild(op);
-
-		var xjhItemLogoWrap = document.getElementsByClassName("xjhItemLogoWrap")[i];
-    	var xjhItemCompanyName = document.getElementsByClassName("xjhItemCompanyName")[i];
-    	var xjhItemSchoolName = document.getElementsByClassName("xjhItemSchoolName")[i];
-    	var xjhItemSchoolAddress = document.getElementsByClassName("xjhItemSchoolAddress")[i];
-    	var xjhItemWeek = document.getElementsByClassName("xjhItemWeek")[i];
-    	var xjhItemTimePoint = document.getElementsByClassName("xjhItemTimePoint")[i];
-
-    	xjhItemLogoWrap.children[0].src = "public/chang-logo.jpeg";
-    	xjhItemCompanyName.innerHTML = talkJSON.rows[i].companyName;
-    	xjhItemSchoolName.innerHTML = "西安";
-    	// talkJSON.rows[i].xjhItemSchoolName
-    	xjhItemSchoolAddress.innerHTML = talkJSON.rows[i].place;
-		// talkJSON.rows[i].xjhItemSchoolAddress
-    	xjhItemWeek.innerHTML = talkJSON.rows[i].holdTime;
-    	xjhItemTimePoint.innerHTML = "(近期)";
-	}
-
-	op = document.createElement("div");
-	op.setAttribute('class', 'loadMore');
-	op.innerHTML = "加载完成";
-	pn.appendChild(op);
 }
 
 /**
@@ -117,5 +143,5 @@ function getArticle () {
 }
 
 window.onload = function () {
-	loadXMLDoc("https://javaapi.changxiaoyuan.com/ht/infor/getTalkInfor.action?province=xa&pageNum=1", getTalkInfor);
+	setTalkInfor();
 }
